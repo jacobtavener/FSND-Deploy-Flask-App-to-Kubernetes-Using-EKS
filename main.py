f56caadabdf55bcfadb838229226eb1bde7d7e24
Jacob Tavener <jatavener@deloitte.co.uk>
2020-01-27 17:40:27 +0000

    removing test comments

--- main.py
+++ main.py
@@ -102,8 +102,6 @@ def decode_jwt():
                 'nbf': data['nbf'] }
     return jsonify(**response)
 
-#test comments for commit
-
 
 def _get_jwt(user_data):
     exp_time = datetime.datetime.utcnow() + datetime.timedelta(weeks=2)
@@ -111,6 +109,7 @@ def _get_jwt(user_data):
                'nbf': datetime.datetime.utcnow(),
                'email': user_data['email']}
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+
 
 if __name__ == '__main__':
     APP.run(host='127.0.0.1', port=8080, debug=True)
